62c955e577940bb87c90438195687b57d4380790
jontygrbac <s3659145@student.rmit.edu.au>
2025-03-07 14:17:44 +1100

    Implemented sign in/up via Clerk

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,34 @@
-const SignUpPage = () => {
-    return (
-        <div>
-            Sign Up Page
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader, Loader2 } from "lucide-react";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2e2a47]">Welcome Back!</h1>
+          <p className="text-base text-[#7E8CA0]">
+            Log in or create account to get back to your dashboard!{" "}
+          </p>
+          <div className="flex items-center justify-center mt-8">
+            <ClerkLoaded>
+              <SignUp path="/sign-up" />
+            </ClerkLoaded>
+            <ClerkLoading>
+              <Loader2 className="animate-spin text-muted-foreground"></Loader2>
+            </ClerkLoading>
+          </div>
         </div>
-
-    )
+      </div>
+      <div className="h-full bg-[#3C3C3C] hidden lg:flex items=center justify-center">
+        <Image
+          src="/logo.svg"
+          height={200}
+          width={200}
+          alt="Tarot logo, a series of lines"
+        />
+      </div>
+    </div>
+  );
 }
-
-export default SignUpPage
